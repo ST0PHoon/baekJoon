b80c44602c1b11fb8920ec97eb34ac0b0688a132
ST0PHoon <codingnb306@naver.com>
2022-09-22 16:53:39 +0900

    상훈이의 여행

---
+++ 3주차/baek_9372.java
@@ -0,0 +1,37 @@
+package JaGu3Week;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class baek_9372 {
+	static int N, M;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		while (T-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+
+			for (int i = 0; i < M; i++) {
+				br.readLine();
+			}
+			sb.append((N-1) + "\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
